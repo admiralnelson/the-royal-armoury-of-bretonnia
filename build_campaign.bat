07fceb6a2adcb433bcadb81e0e4b92f1dc5a23cb
admiralnelson <admiralofinternetmeme@outlook.com>
2023-02-18 23:30:31 +0700

    single click build

--- build_campaign.bat
+++ build_campaign.bat
@@ -4,7 +4,8 @@ setlocal enabledelayedexpansion
 
 if not exist runtime\TypescriptJSON.lua (
     call powershell "[Reflection.Assembly]::LoadWithPartialName("""System.Windows.Forms""");[Windows.Forms.MessageBox]::show("""Cannot find hardcoded reference to runtime\TypescriptJSON.lua, it doesn't exist""", """Compilation Error""", 0, 16)" > NUL
-    goto :pause
+    pause
+    exit /b 1
 )
 
 echo checking for unsupported Typescript construct
@@ -12,13 +13,15 @@ echo checking for unsupported Typescript construct
 call npx eslint .
 if %ERRORLEVEL% NEQ 0 (
     call powershell "[Reflection.Assembly]::LoadWithPartialName("""System.Windows.Forms""");[Windows.Forms.MessageBox]::show("""Please check the console log""", """Compilation Error""", 0, 16)" > NUL
-    goto :pause
+    pause
+    exit /b 1
 )
 
 call npx tstl
 if %ERRORLEVEL% NEQ 0 (
     call powershell "[Reflection.Assembly]::LoadWithPartialName("""System.Windows.Forms""");[Windows.Forms.MessageBox]::show("""Please check the console log""", """Compilation Error""", 0, 16)" > NUL
-    goto :pause
+    pause
+    exit /b 1
 )
 
 
@@ -27,6 +30,4 @@ robocopy script *.lua /s /V /XD .git script node_modules .vscode /XF *.ts > NUL
 mkdir script\_lib\mod 2> NUL
 copy runtime\*.* script\_lib\mod > NUL
 
-:pause
-pause
-
+:end
